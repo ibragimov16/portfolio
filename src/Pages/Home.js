49941a3256966cd3238279561a48948997ac1e6f
Artur <ibragimov.artur608@gmail.com>
2023-06-05 12:19:50 +0300

    added telegram link

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,7 +7,22 @@ const Home = observer(() => {
   return (
     <section className="home-page">
       <div className="content">
-        <div className="title">Hello world</div>
+        <a
+          className="telegram-link"
+          href="https://t.me/ibragimov16"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @ibragimov16
+        </a>
+
+        <div className="title">
+          <h1>
+            A collection of <br /> Front-End projects <br />{' '}
+            <span>by Artur Ibragimov</span>
+          </h1>
+        </div>
+
         <div className="projects-list">
           {projects.map((project, i) => (
             <Project
